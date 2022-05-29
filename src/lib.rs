2a38b33b0b6debe022a028ca3fe687e6e617789a
jorge <jym272@gmail.com>
2022-05-29 19:13:23 -0300

    feat: buffer optimization for the receiver

--- src/lib.rs
+++ src/lib.rs
@@ -53,6 +53,7 @@ impl<T> Drop for Sender<T>{
 }
 struct Receiver<T> {
     shared: Arc<Shared<T>>,
+    buffer: VecDeque<T>,
 }
 
 fn channel<T>() -> (Sender<T>, Receiver<T>) {
@@ -68,33 +69,52 @@ fn channel<T>() -> (Sender<T>, Receiver<T>) {
             shared: shared.clone(),
 
         },
-        Receiver { shared },
+        Receiver {
+            shared,
+            buffer: VecDeque::new(),
+        },
     )
 }
 
 impl<T> Sender<T> {
     fn send(&self, t: T) {
         let mut inner = self.shared.inner.lock().unwrap();
-        inner.queue.push_back(t);
+        inner.queue.push_back(t); //vector resizing(new allocation, move the values, deAllocation) is not blocking
         drop(inner); //drop the lock
         self.shared.available.notify_one(); //wake up the receiver(the thread in the condvar - a receiver) and immediately take the lock and try to receive the message
     }
 }
 
 impl<T> Receiver<T> {
-    fn recv(&self) -> Option<T> {
+    fn recv(&mut self) -> Option<T> {
+        if let Some(t) = self.buffer.pop_front() { //if the buffer is not empty, there is a message in the buffer, we don't have to take the lock
+            return Some(t);
+        }
         let mut inner = self.shared.inner.lock().unwrap();
         loop {
             match inner.queue.pop_front() {
-                Some(t) => return Some(t), //With 'wait' if I'm awake, I take the mutex and check if there is a message, if there is a msg return it, if not I release the mutex and wait again
+                Some(t) =>{
+                    if !inner.queue.is_empty() {
+                        std::mem::swap(&mut self.buffer, &mut inner.queue);
+                    }
+                    return Some(t)}, //With 'wait' if I'm awake, I take the mutex and check if there is a message, if there is a msg return it, if not I release the mutex and wait again
                 None if inner.senders==0   => return None, //if there is no message and there are no senders, return None
                 None => { //Block & wait
                     //you can't wait while still holding the mutex lock.
                     //unwrap -> give up the mutex in order to wait
                     inner = self.shared.available.wait(inner).unwrap(); //just before sleep give away the lock/consume the mutex
+                    //condvar wait needs a mutex guard
                 }
             }
         }
+    }
+}
+
+
+impl<T> Iterator for Receiver<T>{
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.recv()
     }
 }
 
@@ -104,18 +124,24 @@ mod test {
     use super::*;
     #[test]
     fn ping_pong() {
-        let (tx, rx) = channel();
+        let (tx, mut rx) = channel();
         tx.send(1);
         assert_eq!(rx.recv(), Some(1));
     }
 
     #[test]
-    fn closed() {
-        let (tx, rx) = channel::<()>();
+    fn closed_tx() {
+        let (tx, mut rx) = channel::<()>();
         // let _ = tx; //this does not drop the sender
         drop(tx);
         // eprintln!("{:?}", "closed test");
         assert_eq!(rx.recv(), None);
         // let _ = rx.recv(); //drop the receiver, but there's no futures senders
+    }
+    #[test]
+     fn closed_rx() {
+        let (tx, rx) = channel::<()>();
+        drop(rx);
+        tx.send(()); //it should return something?
     }
 }
